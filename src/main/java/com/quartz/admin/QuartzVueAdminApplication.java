b76ae7aac09050f8ec8108e266d04da2a32cf07f
ChoiJoongHyun <joonghyun0117@gmail.com>
2019-07-15 17:15:42 +0900

    프로젝트 생성

---
+++ src/main/java/com/quartz/admin/QuartzVueAdminApplication.java
@@ -0,0 +1,13 @@
+package com.quartz.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzVueAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzVueAdminApplication.class, args);
+    }
+
+}
