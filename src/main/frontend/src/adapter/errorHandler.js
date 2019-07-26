e506528dd0bf862f84ef832cffc335e12280f5cf
ChoiJoongHyun <joonghyun0117@gmail.com>
2019-07-26 18:12:07 +0900

    error message box 수정.

--- src/main/frontend/src/adapter/errorHandler.js
+++ src/main/frontend/src/adapter/errorHandler.js
@@ -6,8 +6,8 @@ export default {
     if(error.response.status === 401) {
       window.location.href = window.location.origin + "/";
     } else {
-      const errorMessage = error.response.data;
-      MessageBox.alert(errorMessage, {type: 'error', dangerouslyUseHTMLString: true});
+      const data = error.response.data;
+      MessageBox.alert(data.errorMsg, 'ERROR', {type: 'error', dangerouslyUseHTMLString: true});
     }
   }
 }
