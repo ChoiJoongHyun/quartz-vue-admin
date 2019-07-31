2e5205fc12a1b3923b8768ad2d8e2d01b883c152
ChoiJoongHyun <joonghyun0117@gmail.com>
2019-07-31 16:12:19 +0900

    HelloWorld 컴포넌트 제거.

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,14 +1,10 @@
 <template>
   <div id="app">
-
     <Layout>
       <keep-alive>
         <router-view></router-view>
       </keep-alive>
     </Layout>
-
-    <!--<img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
